17046e71e75baa783d8c33ec7495579f71152ad5
CameronTwiggs <cameron.020617@gmail.com>
2022-06-30 14:56:14 -0400

    adds express code to app.js

--- Node + Mongo/app.js
+++ Node + Mongo/app.js
@@ -1,19 +1,32 @@
 const {MongoClient, ObjectID} = require('mongodb');
-
-
 const mongoUser = encodeURIComponent('JohnHope');
 const mongoPass = encodeURIComponent('kjpKtE5zwFPt7H2WFzQ4UENjxuDPvfQuKT3');
+
+
+const express = require('express');
+const app = express();
+const port = 8080;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 
 async function main() {
 
     const uri = `mongodb+srv://${mongoUser}:${mongoPass}@hopehacksdatabase.gzz69bj.mongodb.net/?retryWrites=true&w=majority`;
-
-
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
-
+        console.log("database connected");
         await listDatabases(client);
     }
     catch (e) {
@@ -34,4 +47,3 @@ async function listDatabases(client){
         console.log(`- ${db.name}`);
     });
 };
-
