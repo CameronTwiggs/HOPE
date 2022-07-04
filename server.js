0ee20566e0839f121ccb09a1d76ea720b4eb1da4
CameronTwiggs <cameron.020617@gmail.com>
2022-07-04 01:40:55 -0400

    adds monngo

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-// create an express app
+const MongoClient = require('mongodb').MongoClient;
+const mongoUser = encodeURIComponent('JohnHope');
+const mongoPass = encodeURIComponent('kjpKtE5zwFPt7H2WFzQ4UENjxuDPvfQuKT3');
+const uri = `mongodb+srv://${mongoUser}:${mongoPass}@hopehacksdatabase.gzz69bj.mongodb.net/?retryWrites=true&w=majority`;
+const cors = require("cors")
+// starts express server
 const express = require('express');
 const app = express();
 const port = 8080;
-const cors = require("cors")
-// starts express server
-app.use(cors());
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${port}`));
 
-app.get('/', (req, res) => {
-    console.log("Hello World");
-    res.send("Hello World");
-});
+app.use(cors());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+    console.log("Connected to database");
+    const db = client.db('hopehacks');
+    const collection = db.collection('listings');
+   
+    app.get('/', (req, res) => {
+        collection.find().toArray()
+        .then (docs => {
+            res.send(docs);
+        }
+        )
+    }    
+    );
+})
