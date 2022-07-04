083f49a2f4447dcc81d78fc3fc6755a2d31bc9f7
CameronTwiggs <cameron.020617@gmail.com>
2022-07-04 01:25:30 -0400

    rechanges

---
+++ server.js
@@ -0,0 +1,13 @@
+// create an express app
+const express = require('express');
+const app = express();
+const port = 8080;
+const cors = require("cors")
+// starts express server
+app.use(cors());
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/', (req, res) => {
+    console.log("Hello World");
+    res.send("Hello World");
+});
