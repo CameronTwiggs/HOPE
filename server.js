c420e740a994129aedde4c707870b38f47a9e646
CameronTwiggs <cameron.020617@gmail.com>
2022-07-04 01:53:44 -0400

    adds uri to enviroment vairables

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const MongoClient = require('mongodb').MongoClient;
-const mongoUser = encodeURIComponent('JohnHope');
-const mongoPass = encodeURIComponent('kjpKtE5zwFPt7H2WFzQ4UENjxuDPvfQuKT3');
-const uri = `mongodb+srv://${mongoUser}:${mongoPass}@hopehacksdatabase.gzz69bj.mongodb.net/?retryWrites=true&w=majority`;
 const cors = require("cors")
 // starts express server
 const express = require('express');
@@ -12,7 +9,7 @@ app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
     console.log("Connected to database");
     const db = client.db('hopehacks');
     const collection = db.collection('listings');
