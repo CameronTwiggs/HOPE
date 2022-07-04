e6567f65cf59a8de544f997cd5e3b2e6edf8b702
CameronTwiggs <cameron.020617@gmail.com>
2022-07-04 02:02:30 -0400

    Database connection attempt #3

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Road To Hire H.O.P.E projects
 
 BEM Naming Convention
 https://www.youtube.com/watch?v=er1JEDuPbZQ&t=59s
+
+
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+// connects to mongoDB and inserts a document
 const MongoClient = require('mongodb').MongoClient;
 const cors = require("cors")
-// starts express server
 const express = require('express');
+
+
 const app = express();
 const port = 8080;
 
 app.use(cors());
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${port}`));
 
 MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, client) => {
     console.log("Connected to database");
