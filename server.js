ab9504fd7f80e931bb35d280dfd5eb9f67289dda
CameronTwiggs <cameron.020617@gmail.com>
2022-07-04 01:32:13 -0400

    fixes port issues with heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // create an express app
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 const cors = require("cors")
 // starts express server
 app.use(cors());
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${port}`));
 
 app.get('/', (req, res) => {
     console.log("Hello World");
