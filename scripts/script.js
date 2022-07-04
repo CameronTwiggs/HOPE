ad68c80cfe64bada4270595b6632e8efb31288a3
CameronTwiggs <cameron.020617@gmail.com>
2022-07-04 03:15:07 -0400

    cleans up the files structure

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const bg = document.getElementById('header');
+const counts = document.getElementById('counts');
+const line = document.getElementById('line'); 
+
+const imgchange = () => {
+  setTimeout(() => {
+    bg.style.background = "url('./images/freeBR2.jpg')no-repeat center center/cover";
+    line.style.height ="33px"; 
+    counts.innerText = "01";
+  }, 0000);
+   setTimeout(() => {
+    bg.style.background = "url('./images/freeBR.jpg')no-repeat center center/cover";
+      line.style.height ="66px"; 
+      counts.innerText = "02";
+  }, 5000);
+   setTimeout(() => {
+    bg.style.background = "url('./images/freeBR3.jpg')no-repeat center center/cover";
+      line.style.height ="99px"; 
+     counts.innerText = "03";
+  }, 10000);
+}
+setInterval(imgchange, 15000);
+ imgchange();
+
