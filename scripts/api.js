4b03afe50eaecc4a87b74e93acd17a21999a73b5
CameronTwiggs <cameron.020617@gmail.com>
2022-07-05 14:00:12 -0400

    updates representives to accept incomeing infomation from the 3party api

--- scripts/api.js
+++ scripts/api.js
@@ -2,11 +2,11 @@
 async function getRepInfo() {
     let name = document.querySelector(".inpRepName").value;
     let juris = document.querySelector(".inpRepJuris").value;
-    let endpoint = `https://hopehacksapi.herokuapp.com/personSearch/?name=${name}"&juris=${juris}`;
+    let page = document.querySelector(".inpRepPage").value;
+    let endpoint = `https://hopehacksapi.herokuapp.com/personSearch/?name=${name}&juris=${juris}&page=${page}`;
     fetch(endpoint)
     .then(response => response.json())
     .then(data => {console.log(data)})
     .catch(error => console.log(error));
-    
     
 }
