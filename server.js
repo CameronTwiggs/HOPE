2926bdb5c9a38ccbce3337e555d70f31274013be
CameronTwiggs <cameron.020617@gmail.com>
2022-07-05 13:34:50 -0400

    adds node-fetch take 5

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 const MongoClient = require('mongodb').MongoClient;
 const cors = require("cors")
 const bodyParser = require("body-parser")
