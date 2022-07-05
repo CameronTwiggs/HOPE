17af4fe2edb9e9c53d01795a7dbd320da3ea8cb2
CameronTwiggs <cameron.020617@gmail.com>
2022-07-05 12:30:31 -0400

    updates fucntion to fetch rep data

--- scripts/api.js
+++ scripts/api.js
@@ -1,3 +1,12 @@
-async function getRepInfo(params) {
+
+async function getRepInfo() {
+    let name = document.querySelector(".inpRepName").value;
+    let juris = document.querySelector(".inpRepJuris").value;
+    let endpoint = `https://hopehacksapi.herokuapp.com/personSearch/?name=${name}"&juris=${juris}`;
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {console.log(data)})
+    .catch(error => console.log(error));
+    
     
 }
